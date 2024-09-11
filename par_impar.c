b7ab59a8be7dd2ef138fbdd14a8959a972b3153c
Marcos Kim <marcos.skim@senacsp.edu.br>
2024-09-11 13:11:59 +0000

    Arquivo exibe mensagem de par o ímpar para um valor.

---
+++ par_impar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int num;
+    printf ("Digite um número:\n");
+    scanf("%d" ,&num);
+    if (num % 2 == 0){
+        printf("O número é par.");
+    }
+    else{
+        printf("O número é ímpar.");
+    }    
+    return 0;
+}
