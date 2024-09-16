02413448403f8e6dcca95ce915a3f63861e7184e
Marcos Kim <marcos.skim@senacsp.edu.br>
2024-09-16 15:05:18 +0000

    Atividades com array

---
+++ array1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+
+//declaração do array (conjunto de dados) num que guarda 4 números inteiros    
+    int num[] = {10,25,87,91};
+    int pos = 6;
+    //imprimir a primeira posição do array num
+    printf("%d\n" ,num[pos-5]);
+
+return 0;
+}
